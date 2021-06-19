b3f04b007a0a138ecce5cb04656e66513a3be8be
alkovtun <alonakovtun55@gmail.com>
2021-06-19 20:31:56 +0200

    add img

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "ecommerece";
+$db = "sklepJubilerski";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
